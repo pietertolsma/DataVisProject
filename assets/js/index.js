c477906f6fc6552d5b385a77f6b786ed5d630c56
pietertolsma <epieter.tolsma@gmail.com>
2021-11-16 10:28:49 +0100

    Add world demo

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,12 +1,32 @@
 
 const svg = d3.select("#mainWindow");
 
-const data = [30,50,60];
+function handleMouseOver(d, i) {
+    d3.select(this).attr("fill", "#088");
+}
 
-svg.selectAll("circle")
-    .data(data)
-    .join("circle")
-    .attr("r", (d, i) => d)
-    .attr("cx", (d, i) => 50 + i * 120)
-    .attr("cy", 100)
-    .style("color", 'black');
+function handleMouseOut(d, i) {
+    d3.select(this).attr("fill", "#000");
+}
+
+d3.json('./../../data/raw_data/countries.geojson').then((bb) => {
+    let width = 1000, height = 550;
+
+    let projection = d3.geoEqualEarth();
+    projection.fitSize([width, height], bb);
+    let geoGenerator = d3.geoPath()
+        .projection(projection);
+
+    svg.style("Width", width).style("height", height);
+
+    svg.append('g').selectAll('path')
+        .data(bb.features)
+        .join('path')
+        .attr('d', geoGenerator)
+        .on("mouseover", handleMouseOver)
+        .on("mouseout", handleMouseOut)
+        .attr("fill", '#000')
+        .attr('fill-opacity', 0.1)
+        .attr('stroke', '#000')
+        .attr('stroke-opacity', 0.08);
+});
