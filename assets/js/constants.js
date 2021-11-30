59af7c6d8c7ae54fd79930d68badd506df45bfbe
pietertolsma <epieter.tolsma@gmail.com>
2021-11-30 13:08:23 +0100

    Rm scrolljacking

--- assets/js/constants.js
+++ assets/js/constants.js
@@ -1,5 +1,5 @@
 let EU_COUNTRIES = ['Austria', 'Belgium', 'Bulgaria', 'Croatia', 'Republic of Cyprus', 'Czech Republic',
-'Denmark', 'Estonia', 'Finland', 'France', 'Germany', 'Greecy', 'Hungary', 'Ireland', 'Italy',
+'Denmark', 'Estonia', 'Finland', 'France', 'Germany', 'Greece', 'Hungary', 'Ireland', 'Italy',
 'Latvia', 'Lithuania', 'Luxembourg', 'Malta', 'Netherlands', 'Poland', 'Portugal', 'Romania', 'Slovakia', 'Slovenia',
 'Spain', 'Sweden', 'United Kingdom'];
 
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,6 @@
+
+
+// Dirty dirty JQuery
 $(document).ready(() => {
 
     let WIDTH = $(window).width(),
@@ -14,14 +17,15 @@ $(document).ready(() => {
         .style("width", WIDTH / 2 + "px")
         .style("height", HEIGHT + "px");
 
-});
 
-function l(o) {
-    console.log(o);
-}
-// Dirty dirty JQuery
-$(document).ready(function() {
     var sections = $(".pageDrawing").toArray();
+
+
+    setSectionHeights();
+    $(window).resize(() => {
+        WIDTH = $(window).width();
+        HEIGHT = $(window).height();
+    });
 
     var scroll = {
         activeSection: 0,
@@ -31,18 +35,11 @@ $(document).ready(function() {
         target: $(sections[0]).position().top
     };
 
-    function setSizes() {
-        for (var i = 0; i < sections.length; i++) {
-            $(sections[i]).css({
-                top: window.innerHeight * i,
-                height: window.innerHeight,
-                width: window.innerWidth
-            });
-        }
-    }
+    $(window).scroll((e) => {
+        console.log("hi");
+        e.preventDefault();
+    });
 
-    setSizes();
-    $("body").on("resize", setSizes());
 
     function downSection() {
         var positionFromTop = $(sections[scroll.activeSection + 1]).position().top;
@@ -56,49 +53,6 @@ $(document).ready(function() {
         --scroll.activeSection;
     }
 
-    $("body")
-        .hammer({ preventDefault: true })
-        .on("swipe", function(event) {
-            if (event.gesture.direction == "up") {
-                if (scroll.activeSection != sections.length - 1) {
-                    downSection();
-                    l("SWIPED UP");
-                }
-            } else if (event.gesture.direction == "down") {
-                if (scroll.activeSection != 0) {
-                    upSection();
-                    l("SWIPED DOWN");
-                }
-            }
-        });
-
-    $(window).on("scroll", function(e) {
-        e.preventDefault();
-    });
-
-    $(window).on("mousewheel", function(event) {
-        event.preventDefault();
-
-        if (scroll.isThrottled) {
-            return;
-        }
-        scroll.isThrottled = true;
-
-        setTimeout(function() {
-            scroll.isThrottled = false;
-        }, scroll.throttleDuration);
-
-        if (event.originalEvent.wheelDelta > 0) {
-            if (scroll.activeSection === 0) return false;
-            upSection();
-            l("WHEELED DOWN");
-        } else {
-            if (scroll.activeSection >= scroll.sectionCount) return false;
-            downSection();
-            l("WHEELED UP");
-        }
-    });
-
     $(window).keydown(function(e) {
         if (e.keyCode == 40 && scroll.activeSection != sections.length - 1) {
             downSection();
@@ -110,12 +64,12 @@ $(document).ready(function() {
     });
 });
 
-$(function() {
-    $("a[href*=#]").on("click", function(e) {
-        e.preventDefault();
-        $("html, body").animate({ scrollTop: $($(this).attr("href")).offset().top },
-            500,
-            "linear"
-        );
-    });
-});
+// $(function() {
+//     $("a[href*=#]").on("click", function(e) {
+//         e.preventDefault();
+//         $("html, body").animate({ scrollTop: $($(this).attr("href")).offset().top },
+//             500,
+//             "linear"
+//         );
+//     });
+// });
